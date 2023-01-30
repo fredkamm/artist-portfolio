0e678939a324ca38b7d57e6f3366846d7e3e3c09
fredkamm <fred.kamm95@gmail.com>
2023-01-30 14:42:26 -0500

    browser routing is set up, will focus on the navbar and home page next

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/home";
+import About from "./components/about/about";
+import Gallery from "./components/gallery/gallery";
+import Contact from "./components/contact/contact";
+import Layout from "./components/layout/layout";
 import "./App.scss";
-
-import Navbar from "./components/navbar/navbar";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <>
+      <Router>
+        <Routes path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
